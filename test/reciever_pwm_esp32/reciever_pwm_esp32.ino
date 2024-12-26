bb5d808dfc43d56475622d57599179a83c5aed74
HarshKishorani <hkishorani7@gmail.com>
2024-12-26 16:16:37 +0530

    Reciever PWM Test code refractor.

--- test/reciever_pwm_esp32/reciever_pwm_esp32.ino
+++ test/reciever_pwm_esp32/reciever_pwm_esp32.ino
@@ -1,197 +1,143 @@
+// Declare variables to store the previous state of each channel and their timers
 volatile uint32_t current_time;
-volatile uint32_t last_channel_1 = 0;
-volatile uint32_t last_channel_2 = 0;
-volatile uint32_t last_channel_3 = 0;
-volatile uint32_t last_channel_4 = 0;
-volatile uint32_t last_channel_5 = 0;
-volatile uint32_t last_channel_6 = 0;
-volatile uint32_t timer_1;
-volatile uint32_t timer_2;
-volatile uint32_t timer_3;
-volatile uint32_t timer_4;
-volatile uint32_t timer_5;
-volatile uint32_t timer_6;
-volatile int ReceiverValue[6]; // Increase the array size to 6 for Channel 1 to Channel 6
-const int channel_1_pin = 34;
-const int channel_2_pin = 35;
-const int channel_3_pin = 32;
-const int channel_4_pin = 33;
-const int channel_5_pin = 25;
-const int channel_6_pin = 26;
- 
- void channelInterruptHandler()
+volatile uint32_t last_channel_1 = 0, last_channel_2 = 0, last_channel_3 = 0, last_channel_4 = 0;
+volatile uint32_t timer_1, timer_2, timer_3, timer_4;
+
+volatile int receiver_input_channel[4]; // Array to store input values for 4 channels
+
+// Define channel pin numbers
+const int channel_1_pin = 34; // Pin for Channel 1
+const int channel_2_pin = 35; // Pin for Channel 2
+const int channel_3_pin = 32; // Pin for Channel 3
+const int channel_4_pin = 33; // Pin for Channel 4
+
+// Interrupt service routine to handle signal changes for all channels
+void channelInterruptHandler()
 {
-  current_time = micros();
-  // Channel 1
+  current_time = micros(); // Get the current time in microseconds
+
+  // =================== Channel 1 ===================
   if (digitalRead(channel_1_pin))
-  {
+  { // Check if Channel 1 signal is HIGH
     if (last_channel_1 == 0)
-    {
-      last_channel_1 = 1;
-      timer_1 = current_time;
+    {                         // If the previous state was LOW
+      last_channel_1 = 1;     // Update state to HIGH
+      timer_1 = current_time; // Record the time of the rising edge
     }
   }
   else if (last_channel_1 == 1)
-  {
-    last_channel_1 = 0;
-    ReceiverValue[0] = current_time - timer_1;
+  {                                                     // If the signal has gone LOW
+    last_channel_1 = 0;                                 // Update state to LOW
+    receiver_input_channel[0] = current_time - timer_1; // Calculate pulse width
   }
 
-  // Channel 2
+  // =================== Channel 2 ===================
   if (digitalRead(channel_2_pin))
-  {
+  { // Check if Channel 2 signal is HIGH
     if (last_channel_2 == 0)
-    {
-      last_channel_2 = 1;
-      timer_2 = current_time;
+    {                         // If the previous state was LOW
+      last_channel_2 = 1;     // Update state to HIGH
+      timer_2 = current_time; // Record the time of the rising edge
     }
   }
   else if (last_channel_2 == 1)
-  {
-    last_channel_2 = 0;
-    ReceiverValue[1] = current_time - timer_2;
+  {                                                     // If the signal has gone LOW
+    last_channel_2 = 0;                                 // Update state to LOW
+    receiver_input_channel[1] = current_time - timer_2; // Calculate pulse width
   }
 
-  // Channel 3
+  // =================== Channel 3 ===================
   if (digitalRead(channel_3_pin))
-  {
+  { // Check if Channel 3 signal is HIGH
     if (last_channel_3 == 0)
-    {
-      last_channel_3 = 1;
-      timer_3 = current_time;
+    {                         // If the previous state was LOW
+      last_channel_3 = 1;     // Update state to HIGH
+      timer_3 = current_time; // Record the time of the rising edge
     }
   }
   else if (last_channel_3 == 1)
-  {
-    last_channel_3 = 0;
-    ReceiverValue[2] = current_time - timer_3;
+  {                                                     // If the signal has gone LOW
+    last_channel_3 = 0;                                 // Update state to LOW
+    receiver_input_channel[2] = current_time - timer_3; // Calculate pulse width
   }
 
-  // Channel 4
+  // =================== Channel 4 ===================
   if (digitalRead(channel_4_pin))
-  {
+  { // Check if Channel 4 signal is HIGH
     if (last_channel_4 == 0)
-    {
-      last_channel_4 = 1;
-      timer_4 = current_time;
+    {                         // If the previous state was LOW
+      last_channel_4 = 1;     // Update state to HIGH
+      timer_4 = current_time; // Record the time of the rising edge
     }
   }
   else if (last_channel_4 == 1)
-  {
-    last_channel_4 = 0;
-    ReceiverValue[3] = current_time - timer_4;
-  }
-
-  // Channel 5
-  if (digitalRead(channel_5_pin))
-  {
-    if (last_channel_5 == 0)
-    {
-      last_channel_5 = 1;
-      timer_5 = current_time;
-    }
-  }
-  else if (last_channel_5 == 1)
-  {
-    last_channel_5 = 0;
-    ReceiverValue[4] = current_time - timer_5;
-  }
-
-  // Channel 6
-  if (digitalRead(channel_6_pin))
-  {
-    if (last_channel_6 == 0)
-    {
-      last_channel_6 = 1;
-      timer_6 = current_time;
-    }
-  }
-  else if (last_channel_6 == 1)
-  {
-    last_channel_6 = 0;
-    ReceiverValue[5] = current_time - timer_6;
+  {                                                     // If the signal has gone LOW
+    last_channel_4 = 0;                                 // Update state to LOW
+    receiver_input_channel[3] = current_time - timer_4; // Calculate pulse width
   }
 }
 
-
-void neutralPositionAdjustment()
+void print_signals()
 {
-  int min = 1490;
-  int max = 1510;
-  if (ReceiverValue[0] < max && ReceiverValue[0] > min)
-  {
-    ReceiverValue[0]= 1500;
-  } 
-  if (ReceiverValue[1] < max && ReceiverValue[1] > min)
-  {
-    ReceiverValue[1]= 1500;
-  } 
-  if (ReceiverValue[3] < max && ReceiverValue[3] > min)
-  {
-    ReceiverValue[3]= 1500;
-  } 
-  if(ReceiverValue[0]==ReceiverValue[1] && ReceiverValue[1]==ReceiverValue[3] && ReceiverValue[3]==ReceiverValue[0] )
-  {
-    ReceiverValue[0]= 1500;
-    ReceiverValue[1]= 1500;
-    ReceiverValue[3]= 1500;
-  }
+  Serial.print("Channel 1 (Roll): ");
+  if (receiver_input_channel[0] < 1480)
+    Serial.print(" <<< ");
+  else if (receiver_input_channel[0] > 1520)
+    Serial.print(" >>> ");
+  else
+    Serial.print(" -+- ");
+  Serial.print(receiver_input_channel[0]);
+  Serial.print(" us  || ");
+
+  Serial.print("Channel 2 (Nick): ");
+  if (receiver_input_channel[1] < 1480)
+    Serial.print(" ^^^ ");
+  else if (receiver_input_channel[1] > 1520)
+    Serial.print(" vvv ");
+  else
+    Serial.print(" -+- ");
+  Serial.print(receiver_input_channel[1]);
+  Serial.print(" us  || ");
+
+  Serial.print("Channel 3 (Gas): ");
+  if (receiver_input_channel[2] < 1480)
+    Serial.print(" vvv ");
+  else if (receiver_input_channel[2] > 1520)
+    Serial.print(" ^^^ ");
+  else
+    Serial.print(" -+- ");
+  Serial.print(receiver_input_channel[2]);
+  Serial.print(" us  || ");
+
+  Serial.print("Channel 4 (Yaw): ");
+  if (receiver_input_channel[3] < 1480)
+    Serial.print(" <<< ");
+  else if (receiver_input_channel[3] > 1520)
+    Serial.print(" >>> ");
+  else
+    Serial.print(" -+- ");
+  Serial.println(receiver_input_channel[3]);
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
 
-int led_time=100;
- pinMode(15, OUTPUT);
-  digitalWrite(15, LOW);
-  delay(led_time);
-  digitalWrite(15, HIGH);
-  delay(led_time);
-  digitalWrite(15, LOW);
-  delay(led_time);
-  digitalWrite(15, HIGH);
-  delay(led_time);
-  digitalWrite(15, LOW);
-  delay(led_time);
-  digitalWrite(15, HIGH);
-  delay(led_time);
-  digitalWrite(15, LOW);
-  delay(led_time);
-  digitalWrite(15, HIGH);
-  delay(led_time);
-  digitalWrite(15, LOW);
-  delay(led_time);
-
-
+  // Set channel pins as input with internal pull-up resistors
   pinMode(channel_1_pin, INPUT_PULLUP);
   pinMode(channel_2_pin, INPUT_PULLUP);
   pinMode(channel_3_pin, INPUT_PULLUP);
   pinMode(channel_4_pin, INPUT_PULLUP);
-  pinMode(channel_5_pin, INPUT_PULLUP);
-  pinMode(channel_6_pin, INPUT_PULLUP);
 
+  // Attach interrupts to monitor signal changes
   attachInterrupt(digitalPinToInterrupt(channel_1_pin), channelInterruptHandler, CHANGE);
   attachInterrupt(digitalPinToInterrupt(channel_2_pin), channelInterruptHandler, CHANGE);
   attachInterrupt(digitalPinToInterrupt(channel_3_pin), channelInterruptHandler, CHANGE);
   attachInterrupt(digitalPinToInterrupt(channel_4_pin), channelInterruptHandler, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(channel_5_pin), channelInterruptHandler, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(channel_6_pin), channelInterruptHandler, CHANGE);
-  
 }
 
-void loop() {
-
-//Reciever signals
-  Serial.print(ReceiverValue[0]);
-  Serial.print(" - ");
-  Serial.print(ReceiverValue[1]);
-  Serial.print(" - ");
-  Serial.print(ReceiverValue[2]);
-  Serial.print(" - ");
-  Serial.print(ReceiverValue[3]);
-  Serial.print(" - ");
-  Serial.print(ReceiverValue[4]);
-  Serial.print(" - ");
-  Serial.print(ReceiverValue[5]);
-  Serial.println(" -- ");
+void loop()
+{
+  delay(500);
+  print_signals();
 }
